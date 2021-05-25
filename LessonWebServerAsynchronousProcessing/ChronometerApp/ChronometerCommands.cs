5c70afd3de6530d9da4f3a8b7af00e03a0fdcf91
kkaraivanov <kostadin.karaivanov@outlook.com>
2021-05-25 10:56:54 +0300

    refactoring

---
+++ LessonWebServerAsynchronousProcessing/ChronometerApp/ChronometerCommands.cs
@@ -0,0 +1,47 @@
+﻿namespace ChronometerApp
+{
+    using System;
+
+    public static class ChronometerCommands
+    {
+        public static void SetChronometerCommand(this string command, IChronometer chronometer)
+        {
+            switch (command)
+            {
+                case "start":
+                    chronometer.Start();
+                    break;
+                case "stop":
+                    chronometer.Stop();
+                    break;
+                case "lap":
+                    var lap = chronometer.Lap();
+
+                    if (!string.IsNullOrEmpty(lap))
+                    {
+                        Console.WriteLine(lap);
+                    }
+
+                    break;
+                case "laps":
+                    Console.WriteLine(chronometer.Laps.Count == 0
+                        ? "Laps: No laps"
+                        : "Laps:" + Environment.NewLine + string.Join(Environment.NewLine, chronometer.Laps));
+                    break;
+                case "time":
+                    var time = chronometer.GetTime;
+                    if (!string.IsNullOrEmpty(time))
+                    {
+                        Console.WriteLine(time);
+                    }
+                    break;
+                case "reset":
+                    chronometer.Reset();
+                    break;
+                case "exit":
+                    return;
+                    break;
+            }
+        }
+    }
+}
