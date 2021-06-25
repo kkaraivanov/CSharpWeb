034e57d6001aa79a2dbbdc8004d2d3f4d5e39980
kkaraivanov <kostadin.karaivanov@outlook.com>
2021-06-25 22:09:58 +0300

    code refactoring

--- WebBasics/Exam2606220/ExamHelper/ExamHelperLibrary/Services/IValidator.cs
+++ WebBasics/Exam2606220/ExamHelper/ExamHelperLibrary/Services/IValidator.cs
@@ -1,7 +1,6 @@
 ﻿namespace ExamHelperLibrary.Services
 {
     using System.Collections.Generic;
-    using System.Linq;
 
     public interface IValidator
     {
@@ -10,28 +9,5 @@
         IReadOnlyCollection<string> Errors { get; }
 
         void AddError(string error);
-    }
-
-    public class Validator : IValidator
-    {
-        private readonly ObjectAttributeValidator _validator;
-        private ValidatorResult _modelValidate;
-
-        public bool ModelIsValid(object obj) =>
-            GetModelValidateResult(obj).IsValid;
-
-        public IReadOnlyCollection<string> Errors =>
-            _modelValidate.Errors.ToList();
-
-        public void AddError(string error) =>
-            _modelValidate.Errors.Add(error);
-
-        public Validator()
-        {
-            _validator = new ObjectAttributeValidator();
-        }
-
-        private ValidatorResult GetModelValidateResult(object obj) =>
-            _modelValidate = _validator.Validate(obj);
     }
 }
---
+++ WebBasics/Exam2606220/ExamHelper/ExamHelperLibrary/Services/Validator.cs
@@ -0,0 +1,28 @@
+﻿namespace ExamHelperLibrary.Services
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Validator : IValidator
+    {
+        private readonly ObjectAttributeValidator _validator;
+        private ValidatorResult _modelValidate;
+
+        public bool ModelIsValid(object obj) =>
+            GetModelValidateResult(obj).IsValid;
+
+        public IReadOnlyCollection<string> Errors =>
+            _modelValidate.Errors.ToList();
+
+        public void AddError(string error) =>
+            _modelValidate.Errors.Add(error);
+
+        public Validator()
+        {
+            _validator = new ObjectAttributeValidator();
+        }
+
+        private ValidatorResult GetModelValidateResult(object obj) =>
+            _modelValidate = _validator.Validate(obj);
+    }
+}
